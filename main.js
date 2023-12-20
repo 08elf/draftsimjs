09b045939b4b8e4a7615b7e24aaabbf223588df3
Adam Stewart <adza081@gmail.com>
2023-12-20 17:24:57 +1100

    selectplayer table bug fix & UI update

--- main.js
+++ main.js
@@ -288,18 +288,13 @@ async function createSelectPlayerTable(availablePlayers, flag) {
         paging: false,
         lengthChange: false,
         info: false,
-        order: [[3, 'desc']],
+        order: [[1, 'desc']],
         columnDefs: [
-            { targets: [0], title: "Player" },
-            { targets: [1], title: "Pos" },
-            { targets: [2], title: "Age" },
-            { targets: [3], title: "Avg" },
-            { 
-                targets: [4], 
-                title: "ADP",
-                type: "adp-sort" // Use the custom sorting type for ADP
-            },
-            { targets: [5], title: "PlayerId", visible: false },
+            { targets: [0], title: "Player", className: "no-wrap" },
+            { targets: [1], title: "Avg", className: "dt-center" },
+            { targets: [2], title: "ADP", className: "dt-center", type: "adp-sort" },
+            { targets: [3], title: "PlayerId", visible: false },
+            { targets: [4], title: "Pos", visible: false },
         ],
     });
 
@@ -307,13 +302,14 @@ async function createSelectPlayerTable(availablePlayers, flag) {
 
     availablePlayers.forEach(player => {
         const playerAdp = adpData.find(p => p.player_id === player.player_id)?.adp || 'N/A';
+        const formattedPlayer = `<div>${player.name}</div><div style="font-size: smaller;"><b>Position: ${player.positions.join(' | ')}</b> (Age: ${calculateAge(player.dob)})</div>`;
+
         selectPlayerTable.row.add([
-            player.name,
-            player.positions.join(', '),
-            calculateAge(player.dob),
+            formattedPlayer,
             player.fantasy_average,
-            playerAdp, // Add the ADP here
-            player.player_id
+            playerAdp,
+            player.player_id,
+            player.positions.join(', ')
         ]);
     });
 
@@ -321,7 +317,7 @@ async function createSelectPlayerTable(availablePlayers, flag) {
 
     $('#positionFilter').on('change', function() {
         var searchTerm = this.value ? this.value : '';
-        selectPlayerTable.column(1).search(searchTerm).draw();
+        selectPlayerTable.column(4).search(searchTerm).draw();
     });
 
     if (flag) {
@@ -351,8 +347,9 @@ async function createSelectPlayerTable(availablePlayers, flag) {
             };
     
             $('#selectPlayerTable tbody').on('click', 'tr', function () {
+                const rowId = selectPlayerTable.row(this).id();
                 const rowData = selectPlayerTable.row(this).data();
-                const playerName = rowData[0];
+                const playerName = rowData[6];
                 selectedPlayerId = rowData[5];
                 $('#selectPlayer').html(`Do you want to select ${playerName}?`);
                 modal.show();
@@ -735,7 +732,6 @@ function updatePickLog(log) {
             { targets: [1], title: "Players" },
             { targets: [2], title: "Position" },
         ],
-        // Set initial order - this will apply when the table is first initialized
         order: [[0, 'desc']]
     });
 
@@ -743,14 +739,12 @@ function updatePickLog(log) {
 
     log.forEach((player, i) => {
         if (i === 0) {
-            // Skip if required based on your logic
             return true;
         } else {
             pickLogTable.row.add([player.pick, player.name, player.position]);
         }
     });
 
-    // Reapply the order every time the table is updated
     pickLogTable.order([[0, 'desc']]).draw();
 }
 
